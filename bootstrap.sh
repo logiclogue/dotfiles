3014d234a14c4bca148904639c93098ec7524cde
logiclogue <jordanpaullord@hotmail.co.uk>
2017-05-01 14:01:42 +0100

    Updates bootstrap to not symlink directories, but create them instead

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")" || exit;
+current_dir="$( pwd )/$( dirname "${BASH_SOURCE[0]}" )"
+destination_dir=~
+build_dir=$current_dir/build
+
+cd "$current_dir" || exit;
 
 function doIt() {
-    for F in `ls -A build`; do
-        ln -sf $( pwd )/build/$F ~/
+    mkdir -p "$destination_dir"
+    cd "$build_dir" || return
+
+    for d in $( find . -type d )
+    do
+        mkdir -p "$destination_dir/$d"
     done
+
+    for f in $( find . -type f )
+    do
+        ln -sf "$build_dir/$f" "$destination_dir/$( dirname "$f" )"
+    done
+
+    cd "$current_dir" || return
 }
 
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
